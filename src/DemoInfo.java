891d857da96f15bcb46e7730995f06e2279b02af
YalDarraji <140679762+YalDarraji@users.noreply.github.com>
2023-07-29 11:31:20 -0400

    Update DemoInfo.java

--- src/DemoInfo.java
+++ src/DemoInfo.java
@@ -4,4 +4,8 @@ public class DemoInfo {
 	public static void main(String[] args) {
 		System.out.println("My name is Yasir.....");
 	}
+		public static void add ( int a , int b ){
+
+		System.out.println(a + b);
+	}
 }
